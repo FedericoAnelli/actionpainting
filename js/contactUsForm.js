0988bed2bb6eced00612965d0a66badf5dfc9e10
Federico Anelli <federicoanelli@gmail.com>
2022-07-06 22:28:59 -0300

    Minor improvements

---
+++ js/contactUsForm.js
@@ -0,0 +1,7 @@
+// DOM Elements
+let name = document.getElementById("inputName");
+let company = document.getElementById("inputCompany");
+let phone = document.getElementById("inputPhone");
+let email = document.getElementById("inputEmail");
+let content = document.getElementById("inputContent");
+let sendEmail = document.getElementById("sendEmail");
