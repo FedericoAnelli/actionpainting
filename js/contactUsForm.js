01643f5b17c0ec9081447bc95a6e8bb7407d7ee5
Federico Anelli <federicoanelli@gmail.com>
2022-08-03 15:29:22 -0300

    Contact us email sending now working

--- js/contactUsForm.js
+++ js/contactUsForm.js
@@ -1,7 +1,22 @@
 // DOM Elements
-let name = document.getElementById("inputName");
+let fullName = document.getElementById("inputName");
 let company = document.getElementById("inputCompany");
 let phone = document.getElementById("inputPhone");
 let email = document.getElementById("inputEmail");
 let content = document.getElementById("inputContent");
 let sendEmail = document.getElementById("sendEmail");
+
+function sendmail() {
+        var contactParams = {
+            from_name: fullName.value,
+            from_email: email.value,
+            message: "Phone: "+phone.value+"\nCompany: "+company.value+"\nMessage:\n"+content.value
+        };
+        console.log(contactParams);
+        emailjs.send('service_gkq2fd5', 'template_23zs7jg', contactParams).then(function (res) {})
+}
+
+sendEmail.addEventListener("click", ()=>{
+    sendmail();
+    console.log("Email Sent");  
+    });
